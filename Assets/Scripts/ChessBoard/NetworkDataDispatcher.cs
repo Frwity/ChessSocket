bf273ee89a150b70b225a8ef006675a709f00e22
Sami AMARA <s.amara@student.isartdigital.com>
2021-09-22 11:53:35 +0200

    The data received in the dispatcher is now redirected toward its recipient

--- Assets/Scripts/ChessBoard/NetworkDataDispatcher.cs
+++ Assets/Scripts/ChessBoard/NetworkDataDispatcher.cs
@@ -1,27 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NetworkDataDispatcher : MonoBehaviour
 {
     [SerializeField]
     ChessGameMgr chessGameMgr = null;
 
-    public static void ProcessReceivedMessage(byte[] message)
+    [SerializeField]
+    private GUIMgr inGameUI = null;
+
+    [SerializeField]
+    private OpenLobbyUI openLobbyUI = null;
+
+    [SerializeField]
+    private LobbySearchUI lobbySearchUI = null;
+
+    public void ProcessReceivedMessage(byte[] message)
     {
         ChessSerializer.ChessObject chessObject = ChessSerializer.Deserialize(message);
 
         switch (chessObject.type)
         {
             case ChessSerializer.DataType.NAME:
+                inGameUI.SetOpponentPseudo((string)chessObject.obj);
+                openLobbyUI.FoundOpponent((string)chessObject.obj);
                 break;
             case ChessSerializer.DataType.READY:
+                openLobbyUI.OpponentReady((bool)chessObject.obj);
+                lobbySearchUI.OpponentReady((bool)chessObject.obj);
                 break;
             case ChessSerializer.DataType.BEGIN:
+                chessGameMgr.enabled = true;
+                chessGameMgr.SetIAEnable(false);
+                break;
+            case ChessSerializer.DataType.COLOR:
+                chessGameMgr.SetPlayingAs((bool)chessObject.obj);
                 break;
             case ChessSerializer.DataType.MOVE:
+                chessGameMgr.PlayTurn((ChessGameMgr.Move)chessObject.obj);
                 break;
             default:
+                
                 break;
         }
     }

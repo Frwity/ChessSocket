b0f56b21f0ec394396168d9661d5d6c9d4126322
Gavelle Antony <>
2021-09-22 17:10:31 +0200

    tchat update fonction

--- Assets/Scripts/UI/Chat.cs
+++ Assets/Scripts/UI/Chat.cs
@@ -8,6 +8,19 @@ public class Chat : MonoBehaviour
     private InputField writeField;
     NetworkDataDispatcher dispatcher = null;
 
+    [SerializeField]
+    Text text1;
+    [SerializeField]
+    Text text2;
+    [SerializeField]
+    Text text3;
+    [SerializeField]
+    Text text4;
+    [SerializeField]
+    Text text5;
+    [SerializeField]
+    Text text6;
+
     private void Start()
     {
         dispatcher = FindObjectOfType<NetworkDataDispatcher>();
@@ -22,5 +35,17 @@ public class Chat : MonoBehaviour
         dispatcher.SendChat(message);
 
         writeField.text = "";
+
+        Receive(message);
+    }
+
+    public void Receive(string message)
+    {
+        text6.text = text5.text;
+        text5.text = text4.text;
+        text4.text = text3.text;
+        text3.text = text2.text;
+        text2.text = text1.text;
+        text1.text = message;
     }
 }
